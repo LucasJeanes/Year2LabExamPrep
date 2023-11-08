93f826318a48ef8f0a286547e2db9f16fd34cb55
G00410024@atu.ie <g00410024@atu.ie>
2023-11-08 11:24:53 +0000

    Program Initialise

---
+++ src/main/java/ie/atu/year2labexamprep/Year2LabExamPrepApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.year2labexamprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Year2LabExamPrepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Year2LabExamPrepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/year2labexamprep/Year2LabExamPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.year2labexamprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Year2LabExamPrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
